937d9342a3d64c171f81fd5bbddbedaed83c563b
mmshooppy-ux <YOUR_EMAIL@DOMAIN.COM>
2026-08-17 09:22:32 +0530

    updates

--- frontend/src/features/Footer.jsx
+++ frontend/src/features/Footer.jsx
@@ -52,7 +52,7 @@ const Footer = () => {
                             </a>
                         </div>
 
-                        <a href="tel:">+91 9496016555</a>
+                        {/* <a href="tel:">+91 9496016555</a> */}
                         <a href="mailto:info@falconindia.co.in">
                         </a>
 
